5f71c5cc085595fd4aa36e90b48a21860b054581
fishbone <fishbone@Mr.Ping>
2019-06-18 22:01:12 +0700

    Socket client UDP

--- ElectricPop/UDPClient.py
+++ ElectricPop/UDPClient.py
@@ -1,23 +1,9 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
 from module.pmread import register_reading
 from threading import Thread
 from cryptography.fernet import Fernet
 import sqlite3 as sql
-import os, re, time, serial, pickle
+import os, re, time, serial, pickle, socket
 import RPi.GPIO as GPIO
-
-cwd = os.path.dirname(os.path.realpath(__file__))
-dbpath = cwd + "/config.db"
-key = b'ztpn8wdO3ZNiNW3V9GZJlKWy8RioHnPC5-W5TQ0ZSEM='
-cipher = Fernet(key)
-counter = 0
-connect_counter = 0
-phone = ''
-
-C_PWpin = 27  # chan C_PW dieu khien nguon cap cho RPI Sim808 Shield
-PWKpin = 17  # chan PWK : bat/tat RPI Sim808 Shield
-swPin = 12  # chan swPin dong ngat relay
 
 # Cai dat cong ket noi Serial
 ser = serial.Serial(port='/dev/ttyS0',
@@ -26,12 +12,32 @@ ser = serial.Serial(port='/dev/ttyS0',
                     stopbits=serial.STOPBITS_ONE,
                     bytesize=serial.EIGHTBITS,
                     timeout=1)
-# setup GPIO
+
+# path to config file
+cwd = os.path.dirname(os.path.realpath(__file__))
+dbpath = cwd + "/config.db"
+
+# Encrypt/Decrypt package
+key = b'ztpn8wdO3ZNiNW3V9GZJlKWy8RioHnPC5-W5TQ0ZSEM='
+cipher = Fernet(key)
+
+# Pin GPIO and setup
+C_PWpin = 27  # chan C_PW dieu khien nguon cap cho RPI Sim808 Shield
+PWKpin = 17  # chan PWK : bat/tat RPI Sim808 Shield
+swPin = 12  # chan swPin dong ngat relay
+
+# Counter variable
+counter = 0
+connect_counter = 0
+phone = ''
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(C_PWpin, GPIO.OUT)
 GPIO.setup(PWKpin, GPIO.OUT)
 GPIO.setup(swPin, GPIO.OUT)
 
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+s.settimeout(3)
 
 # Ham bat/tat modem
 def GSM_Power():
@@ -41,7 +47,6 @@ def GSM_Power():
     time.sleep(2)
     print("Switched\n")
     return
-
 
 # Ham khoi tao cho modem
 def GSM_Init():
@@ -57,7 +62,6 @@ def GSM_Init():
     ser.write(b'AT+CGNSPWR=1\r\n')  # Bat GPS
     time.sleep(1)
     return
-
 
 # KIem tra xem modem dang bat hay tat cho den khi da bat.
 def GSM_Check():
@@ -88,31 +92,6 @@ def GSM_MakeSMS(phone, text):
     return
 
 
-# Ket noi toi serser
-def SRV_Connect():
-    print("Connecting to server...")
-    ser.write(b"AT+CIPCLOSE\r\n")
-    time.sleep(1)
-    ser.write(b"AT+CIPSHUT\r\n")
-    time.sleep(1)
-    config = getrec("config", True)
-    cmd = 'AT+CIPSTART="UDP","{}","{}"'.format(config["server"], config["port"])
-    ser.write(bytes(cmd + '\r\n', encoding="latin1"))
-    time.sleep(1)
-    res = ser.readall()
-    print(str(res, encoding="latin1"))
-
-
-# Gui ban tin den server
-def CIP_Send(packg):
-    ser.write(bytes("AT+CIPSEND\r\n", encoding="latin1"))
-    time.sleep(0.1)
-    ser.write(b'808' + cipher.encrypt(packg))
-    ser.write(b"\x1A")
-    print()
-
-
-# Lay cac ban ghi hoac 1 ban ghi trong sqlite
 def getrec(table, mode=False):
     db = sql.connect(dbpath)
     db.row_factory = sql.Row
@@ -126,7 +105,6 @@ def getrec(table, mode=False):
         res = dict(rows)
     db.close()
     return res
-
 
 # Xu li cac phan hoi tu server
 def recv_package(decrespone):
@@ -163,60 +141,38 @@ def recv_package(decrespone):
         exec(f)
 
 
+def uicosfi_package(token):
+    pack2send = dict()
+    pminfo = getrec("powermeter")
+
+    uicosfi = [register_reading(d["ids"], 2, d["a"], d["a1"], d["a2"], d["a3"], d["vll"], d["vln"], d["v1"], d["v2"],d["v3"], d["v12"], d["v23"], d["v31"], d["pf"], d["pf1"], d["pf2"], d["pf3"]) for d in pminfo]
+    pack2send["record"] = uicosfi
+    pack2send["token"] = token
+
+    packg = b'808' + cipher.encrypt(pickle.dumps(pack2send))
+    return packg
+
 def main():
-    global connect_counter
-    GSM_Check()
-    try:
-        SRV_Connect()
+    config = getrec("config", True)
+    srv_info = (config["server"], config["port"])
+    while True:
+        x_start = time.time()
+        s.sendto(uicosfi_package(config['token']), srv_info)
+        try:
+            res = s.recvfrom(1024)
+            data = pickle.loads(cipher.decrypt(res[3:]))
+            recv_package(data)
+        except socket.timeout:
+            print('Time out to receive data from server ! Try again !')
+        except KeyboardInterrupt:
+            print('Cancel by keyboard')
+        except Exception:
+            print('error to decode data from server !')
 
-        while True:
-            x_start = time.time()
-            pack2send = dict()
-            pminfo = getrec("powermeter")
-            token = getrec("config", True)["token"]
-
-            uicosfi = [
-                register_reading(d["ids"], 2, d["a"], d["a1"], d["a2"], d["a3"], d["vll"], d["vln"], d["v1"], d["v2"],
-                                 d["v3"], d["v12"], d["v23"], d["v31"], d["pf"], d["pf1"], d["pf2"], d["pf3"]) for d in
-                pminfo]
-            pack2send["record"] = uicosfi
-            pack2send["token"] = token
-
-            packg = pickle.dumps(pack2send)
-
-            CIP_Send(packg)
-
-            time.sleep(1)
-
-            res = max(ser.readlines(), key=len)
-            res = res if len(res) > 30 else ''
-
-            if res:
-                try:
-                    decrespone = pickle.loads(cipher.decrypt(res[3:]))
-                    Thread(target=recv_package, args=(decrespone,)).start()
-                except Exception as e:
-                    print('incorect response server data !')
-                finally:
-                    connect_counter = 0
-            else:
-                connect_counter += 1
-
-            print("Time Counter Per Send: {}s".format(time.time() - x_start))
-
-            if connect_counter >= 5:
-                SRV_Connect()
-                connect_counter = 0
-
-    except KeyboardInterrupt:
-        ser.write(b"AT+CIPCLOSE\r\n")
-        ser.close()
-    finally:
-        print("End!\n")
-        GSM_Power()
-        ser.close()
-        GPIO.cleanup((27,17))
-
-
-if __name__ == "__main__":
+        time.sleep(1)
+            
+if __name__ == '__main__':
     main()
+  
+
+
