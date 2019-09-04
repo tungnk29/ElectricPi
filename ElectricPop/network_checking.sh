14e9fdb3d5f7b5ce60fcc15756304fb9c19f31fe
huyddph03676 <huyddph03676@fpt.edu.vn>
2019-09-04 13:36:21 +0700

    #

--- ElectricPop/network_checking.sh
+++ ElectricPop/network_checking.sh
@@ -2,22 +2,26 @@
 
 while true ; do
         ping -c3 -I eth0 8.8.8.8
-        if [ $? == 0 ] ; then
+        if [ $? == 0 ] 
+        then
                 echo "Eth0 is working"
 
                 current_dni=$(ip route show | awk '/default.*ppp0/ {print $3}')
-                if [$current_dni == "ppp0"] ; then
+                if [ $current_dni == "ppp0" ]
+                then
                     echo "Switching to Ethernet"
                     sudo ip route del default dev ppp0
+                fi
         else
                 echo "Eth0 is busted.\n"
 
                 current_dni=$(ip route show | awk '/default.*ppp0/ {print $3}')
-                if [$current_dni != "ppp0"] ; then
+                if [[ $current_dni != "ppp0" ]] ; then
                     # check connection over ppp0 interface
                     ping -c3 -I ppp0 8.8.8.8
 
-                    if [ $? == 0] ; then
+                    if [ $? == 0 ]
+                    then
                         echo "Switching to USB 3G..."              
                         sudo ip route add default dev ppp
                     else
