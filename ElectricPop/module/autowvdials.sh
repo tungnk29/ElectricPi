50cdae76f623817f9f21a15122fdee21ea11bc59
huyddph03676 <huyddph03676@fpt.edu.vn>
2019-09-04 16:50:55 +0100

    fix detect USB 3G port

---
+++ ElectricPop/module/autowvdials.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+net_port=$(dmesg | awk '/GSM.*ttyUSB[0-9]/ {print $NF}' | head -1)
+
+if [[ $net_port != '' ]] ; then
+    echo "Found path of GSM Modem ! Setting up config file !"
+    sed -i "s|tty.*|$net_port|" /etc/wvdial.conf
+    echo "Staring wvdial service"
+    wvdial
+else
+    echo "GSM modem not found or permission denied !"
+fi
