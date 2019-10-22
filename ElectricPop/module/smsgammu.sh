fa915f5b3a2ad6d6d093dc6b536f7a802a999931
huyddph03676 <huyddph03676@fpt.edu.vn>
2019-10-22 15:01:57 +0700

    FIX send sms v1

--- ElectricPop/module/smsgammu.sh
+++ ElectricPop/module/smsgammu.sh
@@ -5,7 +5,7 @@ net_sms=$(dmesg | awk '/GSM.*ttyUSB[0-9]/ {print $NF}' | tail -1)
 if [[ $net_sms != '' ]] ; then 
     echo "Found path of GSM Modem !\n Starting send SMS!"
     sed -i "s|tty.*|$net_sms|" ~/.gammurc
-    echo "$2" | gammu --sendsms TEXT "$1"
+    echo $2 | gammu --sendsms TEXT $1
 else
     echo "Cannot send SMS! Please try again !"
 fi
