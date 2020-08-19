802f84be834a2372bb8842678d40e9d47b24ea17
huyddph03676 <huyddph03676@fpt.edu.vn>
2020-08-19 22:16:51 +0700

    refactor code, moduler function

---
+++ ElectricPop/module/IoTClient.py
@@ -0,0 +1,229 @@
+# -*- coding: utf-8 -*-
+from pymodbus.client.sync import ModbusSerialClient as ModbusClient
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder
+from cryptography.fernet import Fernet
+from redis import Redis
+from datetime import datetime
+import socket, os, subprocess, psutil, time, pickle, json
+import sqlite3 as sql
+import Adafruit_DHT as DHT
+
+reds = Redis(host='localhost', port=6379, db=0)
+
+GPIO_PIN_IN = {
+    'RMU_C_II': 2,
+    'RMU_C_FUSE_CB': 3,
+    'RMU_C_Out': 4,
+    'RMU_P_Air_Low': 5,
+    'RMU_F_Cable_SMS': 6,
+    'MBA_P_HIGHT': 7,
+    'MBA_Oil_HIGHT': 8,
+    'MBA_Oil_LOW': 9,
+    'MBA_T_Over': 10,
+    'MBA_SPARE': 11,
+    'ATM_LV_CI_OP': 12,
+    'DOOR_F_ALaRM': 13,
+}
+
+GPIO_PIN_OUT = {
+    'ATM_CLOSE_1': 17,
+    'ATM_CLOSE_2': 18,
+    'ATM_OPEN_1': 19,
+    'ATM_OPEN_2': 20
+}
+
+try:
+    import RPi.GPIO as GPIO
+    # Setup GPIO mode
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(False)
+
+    for val in GPIO_PIN_IN.values():
+        GPIO.setup(val, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    for val in GPIO_PIN_OUT.values():
+        GPIO.setup(val, GPIO.OUT)
+except:
+    pass
+
+
+
+class PiMethods():
+    CWD = os.path.dirname(os.path.realpath(__file__))
+    DBPATH = CWD + "/config.db"
+
+    TOKEN = reds.get('TOKEN').decode()
+    KEY = reds.get('KEYCRYPT')
+    CIPHER = Fernet(KEY)
+
+
+    COUNTER = 0
+    CONNECT_COUNTER = 0
+    PHONE = ''
+
+    def getrec(self, table, mode=False):
+        db = sql.connect(self.DBPATH)
+        db.row_factory = sql.Row
+        mouse = db.cursor()
+        mouse.execute("SELECT * FROM {}".format(table))
+        if not mode:
+            rows = mouse.fetchall()
+            res = [dict(d) for d in rows]
+        else:
+            rows = mouse.fetchone()
+            res = dict(rows)
+        db.close()
+        return res
+
+    def read_status_pin(self, pin):
+        return GPIO.input(pin)
+
+    def switch_pop(self, boolean = 1):
+        if boolean:
+            GPIO.output(GPIO_PIN_OUT['ATM_CLOSE_1'], 1)
+            time.sleep(2)
+            GPIO.output(GPIO_PIN_OUT['ATM_CLOSE_1'], 0)
+            time.sleep(2)
+        else:
+            GPIO.output(GPIO_PIN_OUT['ATM_OPEN_1'], 1)
+            time.sleep(2)
+            GPIO.output(GPIO_PIN_OUT['ATM_OPEN_1'], 0)
+            time.sleep(2)
+
+        print('Pop status switched\n')
+
+    def GSM_MakeSMS(self, phone, text):
+        os.system(f"bash {self.CWD}/smsgammu.sh '{text}' {phone}")
+
+    def parse_sms(self, content):
+        # full package from server
+        # {
+        #         'address': 'Công Ty TNHH KHC',
+        #         'detail': {
+        #             '1': {'vln': [1.000000045813705e-18, False]}
+        #         }
+        # }
+
+        text = f'Dia chi: {content["address"]} \n'
+        for k, v in content['detail'].items():
+            text += f'Powermeter {k}: '
+            if v.get('vln', False):
+                text += f'VLN: {v["vln"][0]} ({">= Vmax" if v["vln"][1] else "< Vmin"}).'
+            if v.get('a', False):
+                text += f'I: {v["a"][0]} ({">= Imax" if v["a"][1] else "< Imin"}).'
+        return text
+
+    def recv_package(self, decrespone):
+        print(decrespone)
+
+        def alarm():
+            '''Nhan lenh gui tin nhan canh bao'''
+
+            self.PHONE = decrespone["phone"]
+            print(COUNTER)
+            if decrespone.get("alarm") == True:
+                if self.COUNTER % 4 == 0 and self.COUNTER <= 12:
+                    self.GSM_MakeSMS(self.PHONE, parse_sms(decrespone['warning']))
+                    print("Canh bao nguy hiem")
+                self.COUNTER += 1
+
+        def notify(phone):
+            GSM_MakeSMS(phone, "Da het su co !")
+
+        def switch():
+            '''Dong ngat mach'''
+
+            if decrespone["switch"] != GPIO.input(GPIO_PIN_IN['ATM_LV_CI_OP']):
+                self.switch_pop(decrespone["switch"])
+
+            if not decrespone.get("alarm", 0):
+                if self.COUNTER > 0 and self.COUNTER % 4 != 0:
+                    print(phone)
+                    notify(phone=phone)
+                    self.COUNTER = 0
+
+        for f in decrespone['func']:
+            exec(f)
+
+    def uicosfi_package(self, encrypt=True):
+        pack2send = dict()
+        pminfo = self.getrec("powermeter")
+
+        pins_status = dict()
+
+        uicosfi = [self.register_reading(count=2, **d) for d in pminfo]
+        pack2send["data"] = uicosfi
+        pack2send["token"] = self.TOKEN
+        pack2send["temperature"] = self.sensor_reading()
+        pack2send["createAt"] = str(datetime.now())
+        for key, val in GPIO_PIN_IN.items():
+            pins_status[key] = self.read_status_pin(val)
+
+        pack2send.update({ 'status': pins_status })
+
+
+        if encrypt:
+            return self.CIPHER.encrypt(json.dumps(pack2send).encode())
+
+        return json.dumps(pack2send)
+
+    def get_modbus_path(self):
+        shell_path = self.CWD + '/mbdevice.sh'
+
+        cmd = subprocess.Popen("bash " + shell_path, stdout=subprocess.PIPE, shell=True)
+
+        (result, err) = cmd.communicate()
+
+        result = str(result, encoding='utf-8')
+
+        return '/dev/' + result.strip()
+
+    def is_connected(self):
+        try:
+            client = socket.create_connection(('8.8.8.8', 53), 2)
+            client.close()
+            return True
+        except:
+            return False
+
+    def sensor_reading(self, sensor=False):
+        if not sensor:
+            return psutil.sensors_temperatures()['cpu-thermal'][0].current
+
+        humidity,temperature = Adafruit_DHT.read_retry(DHT.DHT11, 14)
+        return temperature
+
+    def validator(self, instance):
+        '''Decode raw data from register'''
+        if not instance.isError():
+
+            decoder = BinaryPayloadDecoder.fromRegisters(
+                instance.registers,
+                byteorder=Endian.Big, wordorder=Endian.Little
+            )
+            return float(decoder.decode_32bit_float())
+
+
+        print("Registers Reading Error!, Try again.")
+        return 0
+
+    def register_reading(self, count, **kwargs):
+        print("Start reading Power Meter...")
+        kwargs.pop('id')
+        result = {
+            'ids': kwargs.pop('ids')
+        }
+        try:
+            modbus = ModbusClient(method='rtu', port=self.get_modbus_path(), baudrate=9600, timeout=1, parity='E', bytesize=8)
+            modbus.connect()
+
+            for key, val in kwargs.items():
+                result[key] = self.validator(modbus.read_holding_registers(val - 1, count, unit=result['ids']))
+                print(key, ': ', result[key]), 
+
+            modbus.close()
+
+            return result
+        except KeyboardInterrupt:
+            print('Ctrl + C pressed or any error')
+
--- ElectricPop/mqtt-pi.py
+++ ElectricPop/mqtt-pi.py
@@ -1,259 +1,74 @@
-from module.pmread import register_reading, sensor_reading
-from cryptography.fernet import Fernet
-from datetime import datetime
-import RPi.GPIO as GPIO
-import sqlite3 as sql
+from module.IoTClient import PiMethods
 import json, time, os, logging
-
 import asyncio
 import signal
 import uvloop
 from gmqtt import Message, Client as MQTTClient
+# from gmqtt.mqtt.constants import MQTTv311
 
 asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
 STOP = asyncio.Event()
 
-# path to database config file
-cwd = os.path.dirname(os.path.realpath(__file__))
-dbpath = cwd + "/config.db"
-
-# Encrypt/Decrypt package
-key = b'ztpn8wdO3ZNiNW3V9GZJlKWy8RioHnPC5-W5TQ0ZSEM='
-cipher = Fernet(key)
-
-# Pin GPIO and setup
-swPinOn = 12  # chan swPinOn dong relay
-swPinOff = 16 # chan swPinOff ngat relay
-statusPin = 18 # chan tiep diem doc trang thai
-
-# Counter variable
-counter = 0
-connect_counter = 0
-phone = ''
-
-# Setup GPIO mode
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
-GPIO.setup(swPinOn, GPIO.OUT)
-GPIO.setup(swPinOff, GPIO.OUT)
-GPIO.setup(statusPin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
-GPIO.output(swPinOn, 0)
-GPIO.output(swPinOff, 0)
-
-def getrec(table, mode=False):
-    '''
-        read infomation from database file
-        mode = fasle ==> return list of dictionary record
-        mode = true ==> return dictionary one record
-    '''
-    db = sql.connect(dbpath)
-    db.row_factory = sql.Row
-    mouse = db.cursor()
-    mouse.execute("SELECT * FROM {}".format(table))
-    if not mode:
-        rows = mouse.fetchall()
-        res = [dict(d) for d in rows]
-    else:
-        rows = mouse.fetchone()
-        res = dict(rows)
-    db.close()
-    return res
-
-# switch on pop
-def switch_pop(boolean = 1):
-    if boolean:
-        GPIO.output(swPinOn, 1)
-        time.sleep(2)
-        GPIO.output(swPinOn, 0)
-        time.sleep(2)
-    else:
-        GPIO.output(swPinOff, 1)
-        time.sleep(2)
-        GPIO.output(swPinOff, 0)
-        time.sleep(2)
-
-    print('Pop status switched\n')
-    return 1
-
-# read status contact pin 
-def read_status_pin():
-    return GPIO.input(statusPin)
-
-# Gui tin nhan
-def GSM_MakeSMS(phone, text):
-    os.system("bash /opt/ElectricPi/ElectricPop/module/smsgammu.sh '{0}' '{1}' &".format(text, phone))
-
-def parse_sms(content):
-    text = 'Dia chi: {} \n'.format(content['address'])
-    for k, v in content['detail'].items():
-        text += '{}: {} \n'.format(k, v)
-    return text
-
-# Xu li cac phan hoi tu server
-def recv_package(decrespone):
-    # print(decrespone)
-
-    def alarm():
-        '''Nhan lenh gui tin nhan canh bao'''
-        global counter, phone
-        phone = decrespone["phone"]
-        print(counter)
-        if decrespone.get("alarm") == True:
-            if counter % 4 == 0 and counter <= 12:
-                GSM_MakeSMS(phone, parse_sms(decrespone['warning']))
-                print("Canh bao nguy hiem")
-            counter += 1
-
-    def notify(phone):
-        GSM_MakeSMS(phone, "Da het su co ! ^ _ ^")
-
-    def switch():
-        '''Dong ngat mach'''
-
-        global phone, counter
-
-        if decrespone["switch"] != GPIO.input(statusPin):
-            # Thread(target=switch_pop, args=(decrespone["switch"],)).start()
-            switch_pop(decrespone["switch"])
-
-        if not decrespone.get("alarm", 0):
-            if counter > 0 and counter % 4 != 0:
-                print(phone)
-                notify(phone=phone)
-                counter = 0
-
-    for f in decrespone['func']:
-        exec(f)
-
-# Data de gui len broker mqtt
-def uicosfi_package(token):
-    pack2send = dict()
-    pminfo = getrec("powermeter")
-
-    uicosfi = [register_reading(d["ids"], 2, d["a"], d["a1"], d["a2"], d["a3"], d["vll"], d["vln"], d["v1"], d["v2"],d["v3"], d["v12"], d["v23"], d["v31"], d["pf"], d["pf1"], d["pf2"], d["pf3"]) for d in pminfo]
-    pack2send["record"] = uicosfi
-    pack2send["token"] = token
-    pack2send["temperature"] = sensor_reading()
-    pack2send["real_status"] = read_status_pin()
-    pack2send["datetime"] = str(datetime.now())
-
-    packg = cipher.encrypt(json.dumps(pack2send).encode())
-    return packg.decode()
+PI = PiMethods()
+TOKEN = PI.TOKEN
 
 
-# Server infomation
-config = getrec("config", True)
-broken_url = 'vscada.ddns.net'
-broken_port = 8883
+topic_status = f'pop/{TOKEN}/status'
+topic_execute = f'pop/{TOKEN}/execute'
+topic_data = f'pop/{TOKEN}/data'
 
-
-topic_status = '/scada/{}/status'.format(config['token'])
-topic_execute = '/scada/{}/execute'.format(config['token'])
-topic_push = '/scada/{}/push'.format(config['token'])
-
-
-rc_message = [
-        'Connection successful',
-        'Connection refused – incorrect protocol version',
-        'Connection refused – invalid client identifier',
-        'Connection refused – server unavailable',
-        'Connection refused – bad username or password',
-        'Connection refused – not authorised'
-    ]
 
 def ask_exit(*args):
-    GPIO.cleanup()
     STOP.set()
 
-def status_package():
-    status_pack = json.dumps({'modem_status': 1, 'pop_status': read_status_pin(), 'token': config['token']}).encode()
-    status_pack = cipher.encrypt(status_pack).decode()
-
-    return status_pack
 
 def on_connect(client, flags, rc, properties):
-    print("connected OK Returned code=",rc)
-
-    client.publish(topic_status, status_package())
-
+    print(f"connected OK Returned code={rc}")
     print(f'subscribing topic {topic_execute} ...')
+    status = json.dumps({'connected': True, 'token': TOKEN})
+    client.publish(topic_status, payload=status, qos=2, retain=1, message_expiry_interval=20)
     client.subscribe(topic_execute, qos=1)
 
 def on_disconnect(client, packet, exc=None):
     print('Disconnected')
 
-def on_subscribe(client, mid, qos):
+def on_subscribe(client, mid, qos, properties):
     print('SUBSCRIBED')
 
 def on_message(client, topic, payload, qos, properties):
     print(f"Message arrive from {topic} ")
-    if (topic == topic_execute):
-        data = json.loads(cipher.decrypt(payload))
-
-        print("Message to execute: " + str(data))
-        
-        recv_package(data)
-
-def on_connect_lwt(client, flags, rc, properties):
-    logging.info('[CONNECTED {}]'.format(client._client_id))
+    data = json.loads(PI.CIPHER.decrypt(payload))
+    PI.recv_package(data)
 
 
-def on_message_lwt(client, topic, payload, qos, properties):
-    logging.info('[RECV MSG {}] TOPIC: {} PAYLOAD: {} QOS: {} PROPERTIES: {}'
-                 .format(client._client_id, topic, payload, qos, properties))
-
-
-def on_disconnect_lwt(client, packet, exc=None):
-    logging.info('[DISCONNECTED {}]'.format(client._client_id))
-
-
-def on_subscribe_lwt(client, mid, qos):
-    logging.info('[SUBSCRIBED {}] QOS: {}'.format(client._client_id, qos))
-
-async def main_push(client):
+async def push_data(client):
     while True:
-        packs = uicosfi_package(config['token'])
-        await asyncio.sleep(1)
-        client.publish(topic_push, payload=packs, qos=1, message_expiry_interval=20)
-        client.publish(topic_status, payload=status_package(), qos=1, message_expiry_interval=20)
+        packs = PI.uicosfi_package()
+        client.publish(topic_data, payload=packs, qos=1, message_expiry_interval=20)
+        await asyncio.sleep(5)
         
         
 async def main():
     # LWT
-    status_lwt = json.dumps({'modem_status': 0, 'pop_status': 0, 'token': config['token']}).encode()
-    status_lwt = cipher.encrypt(status_lwt).decode()
+    status_lwt = json.dumps({'connected': False, 'token': TOKEN})
 
     will_message = Message(topic_status, status_lwt, qos=2, retain=1, will_delay_interval=10)
-    will_client = MQTTClient("", will_message=will_message)
-
-    will_client.on_connect = on_connect_lwt
-    will_client.on_disconnect = on_disconnect_lwt
-    will_client.on_message = on_message_lwt
-    will_client.on_subscribe = on_subscribe_lwt
-
-    will_client.set_auth_credentials('scada', 'Abcd@1234@')
-    await will_client.connect(broken_url, broken_port, ssl=True)
 
     # Main client
-    client = MQTTClient('')
+    client = MQTTClient(client_id=TOKEN, will_message=will_message)
 
     client.on_connect = on_connect
     client.on_disconnect = on_disconnect
     client.on_message = on_message
     client.on_subscribe = on_subscribe
 
-    client.set_auth_credentials('scada', 'Abcd@1234@')
-    await client.connect(broken_url, broken_port, ssl=True)
+    client.set_auth_credentials(TOKEN, TOKEN)
+    await client.connect('vscada.ddns.net', 1883,)
 
-    # client.subscribe(topic=topic_execute, qos=2)
-
-    loop.create_task(main_push(client))
-
-    await will_client.disconnect(reason_code=4, reason_string="Smth went wrong")
+    loop.create_task(push_data(client))
 
     await STOP.wait()
-    await client.disconnect()
+    await client.disconnect(reason_code=4, reason_string="Smth went wrong")
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
